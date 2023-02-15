e84ab143e71d7874c372ff510eaf064bb28bc2aa
Oleg <sirov@ukr.net>
2023-02-15 21:56:26 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = "feedback-form-state";
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 500)); 
+
+getItems(); 
+
+function onFormSubmit(e) {
+    e.preventDefault(); 
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formItems);
+}
+
+const formItems = {};
+
+function onInput() {
+    formItems.email = refs.input.value;
+    formItems.message = refs.textarea.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formItems));
+}
+ 
+function getItems() {
+    const savedItems = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedItems) {
+        refs.input.value = savedItems.email;
+        refs.textarea.value = savedItems.message;
+    }
+}
 
